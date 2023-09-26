704abd3e41afc1910663cf8b75d25e5f669e219f
ToMmY-P8 <pirozhenko92@gmail.com>
2023-09-26 20:40:53 +0300

    Lesson3Mod1

---
+++ Classwork/Lesson03.py
@@ -0,0 +1,35 @@
+# x=int(input('Введіть число :'))
+# if x % 7==0:
+#     print(x, 'Number is ultiple 7')
+# else:
+#     print(x, 'Number is not multiple 7')
+
+# x=int(input('Введіть число #1:'))
+# y=int(input('Введіть число #2:'))
+
+# max=max(x, y)
+# print('Максимум з двох чисел :', max)
+
+# x=int(input('Введіть число #1:'))
+# y=int(input('Введіть число #2:'))
+
+# if x > y:
+#     max = x
+# else:
+#     max = y
+# print ('Максимум з двох чисел :', max)
+
+# x=int(input('Введіть число #1:'))
+# y=int(input('Введіть число #2:'))
+
+# min=min(x, y)
+# print ('Мінімум з двох чисел :', min)
+
+x=int(input('Введіть число #1:'))
+y=int(input('Введіть число #2:'))
+
+if x < y:
+    min = x
+else:
+    min = y
+print ('Максимум з двох чисел :', min)
